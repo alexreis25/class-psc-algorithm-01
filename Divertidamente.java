5ac966469f88f9e92dbf07cfaf37d97bc664b544
alexreis25 <alexvito2012@gmail.com>
2025-09-17 09:00:18 -0300

    Add files via upload

--- Divertidamente.java
+++ Divertidamente.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
+
 public class Divertidamente {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String fezAmizade ;
-        int qtdAmizades , tristeza = 0 , alegria=0,alegria2=0,tristeza2=0;
-        
+        String fezAmizade;
+        int qtdAmizades, tristeza = 0, alegria = 0;
+
         System.out.println("Riley fez amizades na cidade?");
         fezAmizade = scanner.next();
 
-        if(fezAmizade.equals("Sim")){
-             System.out.println("Quantas ela fez");
-             qtdAmizades = scanner.nextInt();
-            alegria=qtdAmizades * 10;
-        }else if(fezAmizade.equals("Não")){
-           tristeza = 30;
+        if (fezAmizade.equals("Sim")) {
+            System.out.println("Quantas ela fez");
+            qtdAmizades = scanner.nextInt();
+            alegria += qtdAmizades * 10;
+        } else if (fezAmizade.equals("Não")) {
+            tristeza += 30;
         }
 
         System.out.println("Digite a primeira nota de Riley: ");
@@ -25,36 +26,30 @@ public class Divertidamente {
         System.out.println("Digite a tercerira nota de Riley: ");
         Double a3 = scanner.nextDouble();
 
-        Double media = (a1 + a2 + a3)/3;
+        Double media = (a1 + a2 + a3) / 3;
 
-        if(media >= 7.00 ){
-         alegria += 50;
-        }else{
-         tristeza2 = 50;
+        if (media >= 7.00) {
+            alegria += 50;
+        } else {
+            tristeza += 50;
         }
 
         System.out.println("Quantos exercicios você conseguiu fazer? ");
         int exercicios = scanner.nextInt();
 
-        int totalExercicios = 10 ;
-        int faltamExercicios = totalExercicios - exercicios ; 
+        int totalExercicios = 10;
+        int faltamExercicios = totalExercicios - exercicios;
 
+        alegria += exercicios * 10;
+        tristeza += faltamExercicios * 10;
 
-        
-        int alegria3 = exercicios * 10;
-        int tristeza3=faltamExercicios*10;
-
-        int alegriaTotal = alegria + alegria2 + alegria3 ; 
-        int tristezaTotal = tristeza + tristeza2 + tristeza3;
-
-        if(alegriaTotal>tristezaTotal){
+        if (alegria > tristeza) {
             System.out.println("A mudança para a nova cidade foi uma experiência incrível para a Riley.");
-        }else{
+        } else {
             System.out.println("A mudança para a nova cidade foi uma experiência desagradável para a Riley.");
         }
 
         scanner.close();
-
 
     }
 }
