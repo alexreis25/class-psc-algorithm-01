cdbde72f66c8989d9018be54abcbda75b314c381
alexreis25 <alexvito2012@gmail.com>
2025-09-16 21:12:02 -0300

    Add files via upload

---
+++ Divertidamente.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+public class Divertidamente {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        String fezAmizade ;
+        int qtdAmizades , tristeza = 0 , alegria=0,alegria2=0,tristeza2=0;
+        
+        System.out.println("Riley fez amizades na cidade?");
+        fezAmizade = scanner.next();
+
+        System.out.println("Se sim, quantas ela fez? ");
+        qtdAmizades = scanner.nextInt();
+
+        if(fezAmizade.equals("Sim")){
+            alegria=qtdAmizades * 10;
+        }else if(fezAmizade.equals("Não")){
+           tristeza = 30;
+        }
+
+        System.out.println("Digite a primeira nota de Riley: ");
+        Double a1 = scanner.nextDouble();
+
+        System.out.println("Digite a segunda nota de Riley: ");
+        Double a2 = scanner.nextDouble();
+
+        System.out.println("Digite a tercerira nota de Riley: ");
+        Double a3 = scanner.nextDouble();
+
+        Double media = (a1 + a2 + a3)/3;
+
+        if(media >= 7.00 ){
+          alegria2 = 50;
+        }else{
+         tristeza2 = 50;
+        }
+
+        System.out.println("Quantos exercicios você conseguiu fazer? ");
+        int exercicios = scanner.nextInt();
+
+        int totalExercicios = 10 ;
+        int faltamExercicios = totalExercicios - exercicios ; 
+
+
+        
+        int alegria3 = exercicios * 10;
+        int tristeza3=faltamExercicios*10;
+
+        int alegriaTotal = alegria + alegria2 + alegria3 ; 
+        int tristezaTotal = tristeza + tristeza2 + tristeza3;
+
+        if(alegriaTotal>tristezaTotal){
+            System.out.println("A mudança para a nova cidade foi uma experiência incrível para a Riley.");
+        }else{
+            System.out.println("A mudança para a nova cidade foi uma experiência desagradável para a Riley.");
+        }
+
+        scanner.close();
+
+
+    }
+}
